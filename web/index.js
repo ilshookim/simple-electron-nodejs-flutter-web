52f494ab1f94860fdc892090bc5a1d4e4b7352c0
ilshookim <ilshookim@gmail.com>
2022-11-12 15:41:12 +0900

    MODIFY app | The application will be exited when middleware port is already used in

--- web/index.js
+++ web/index.js
@@ -1,12 +1,13 @@
 `use strict`;
 
+const Net = require(`net`);
 const Path = require(`path`);
 
 // Modules to logging
 const logger = require('electron-log');
 
 // Modules to control application life and create native browser window
-const { app, screen, BrowserWindow, Menu } = require(`electron`);
+const { app, screen, dialog, BrowserWindow, Menu } = require(`electron`);
 
 // Remove the application menu.
 Menu.setApplicationMenu(null);
@@ -48,18 +49,63 @@ function createWindow() {
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  logger.info('quit - all windows was closed')
-  app.quit()
+  logger.info('quit - all windows was closed');
+  app.quit();
 })
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', async () => {
-  createWindow()
+  const port = process.env.PORT || 8090;
+  const portAlreadyUsedIn = await checkPort(port) !== 'closed';
+  if (portAlreadyUsedIn) {
+    logger.info('quit - middleware port is already used in');
+    dialog.showMessageBoxSync({
+      type: 'warning',
+      message: 'This application cannot be run multiple times',
+      detail: `Middleware port ${port} is already used in`});
+    app.quit();
+    return;
+  }
+  require(`./middleware`);
+  createWindow();
 })
 
 // In this file you can include the rest of your app`s specific main process
 // code. You can also put them in separate files and require them here.
 
-require(`./middleware`);
+async function checkPort(port, host = `localhost`, timeout = 400) {
+  let connectionRefused = false;
+
+  let socket = new Net.Socket();
+  let status = null;
+  let error = null;
+
+  return new Promise((resolve, reject) => {
+    socket.on(`connect`, function onOpen() {
+      status = `open`;
+      socket.destroy();
+    });
+
+    socket.setTimeout(timeout)
+    socket.on(`timeout`, function onTimeout() {
+      status = `closed`;
+      error = new Error(`Timeout (${timeout}ms) occurred waiting for ${host}:${port} to be available`);
+      socket.destroy();
+    });
+
+    socket.on(`error`, function onError(exception) {
+      if (exception.code !== `ECONNREFUSED`) exception = exception;
+      else connectionRefused = true;
+      status = `closed`;
+    });
+
+    socket.on(`close`, function onClose(exception) {
+      if (exception && !connectionRefused) { error = error || exception; } else { error = null; }
+      if (error) reject(error); else resolve(status);
+    });
+
+    socket.connect(port, host);
+  });
+}
